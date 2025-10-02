5b17baed70ade3e2af3cf89a8044f9c5d16d1d5a
Felipe <felipemoura92@gmail.com>
2025-10-02 11:50:06 -0300

    Avançando Next

---
+++ 5 - Fundamentos de Next/Avançando com o Next/cinelista/src/app/page.tsx
@@ -0,0 +1,13 @@
+import { getTrendindMovies } from "@/lib/api/tmdb";
+import Grid from "./components/Grid";
+import Title from "./components/Title"
+
+export default async function Home() {
+  const filmes = await getTrendindMovies();
+  return (
+    <>
+      <Title title="Filmes em destaque"/>
+      <Grid filmes={filmes}/>
+    </>
+  );
+}
