aabb3df6a97d9f9fd63fccaa9d341fe91085b882
Felipe <Felipemoura92@gmail.com>
2025-12-03 00:27:08 -0300

    Meu portfolio exercicio

---
+++ 6 - Testes e Qualidades de Código/Teste de Integração/meu-portfolio/src/App.tsx
@@ -0,0 +1,19 @@
+import "./styles/global.css";
+
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import Projects from "./components/Projects";
+import About from "./components/About";
+import Footer from "./components/Footer";
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <Hero />
+      <Projects />
+      <About />
+      <Footer />
+    </>
+  );
+}
---
+++ 6 - Testes e Qualidades de Código/Teste de Integração/meu-portfolio/src/components/Projects.jsx
@@ -0,0 +1,17 @@
+export default function Projects() {
+  return (
+    <section id="projetos">
+      <h2>Projetos</h2>
+
+      <div>
+        <h3>Projeto 1</h3>
+        <p>Descrição do projeto.</p>
+      </div>
+
+      <div>
+        <h3>Projeto 2</h3>
+        <p>Descrição do projeto.</p>
+      </div>
+    </section>
+  );
+}
